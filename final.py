4ed23e3a8733315d64523dc2aae1746d4a151077
anita04021 <131482579+anita04021@users.noreply.github.com>
2023-04-28 23:27:22 +0200

    Update final.py

--- final.py
+++ final.py
@@ -8,6 +8,23 @@ Created on Thu Apr 27 14:44:36 2023
 import sqlite3
 import pandas as pd
 import streamlit as st
+from sqlite3 import connect
+
+projects = pd.read_excel("projects.xlsx")
+participants = pd.read_excel("participants.xlsx")
+countries = pd.read_excel("countries.xlsx")
+
+connection = connect("ecsel_database.db")
+df_projects = pd.DataFrame(projects)
+df_participants = pd.DataFrame(participants)
+df_countries = pd.DataFrame(countries)
+
+df_projects.to_sql("projects", connection, if_exists = "replace")
+df_participants.to_sql("participants", connection, if_exists = "replace")
+df_countries.to_sql("countries", connection, if_exists = "replace")
+
+
+connection.close()
 
 # create a connection to the database
 conn = sqlite3.connect('ecsel_database.db')
